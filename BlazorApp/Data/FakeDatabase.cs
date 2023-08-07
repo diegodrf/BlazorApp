0dce13a9f6585808b43360bc15f99395aad6ee97
Diego Faria <diego.rdfaria@gmail.com>
2023-08-06 23:10:06 -0400

    Add project files.

---
+++ BlazorApp/Data/FakeDatabase.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorApp.Data
+{
+    public class FakeDatabase
+    {
+        private readonly Dictionary<string, object> _keyValuePairs = new();
+
+        public void SetValue<T>(string key, T value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            _keyValuePairs[key] = value;
+        }
+
+        public T GetValueOrDefault<T>(string key, T defaultValue)
+        {
+            if(_keyValuePairs.TryGetValue(key, out var value))
+            {
+                return (T)value;
+            }
+            
+            return defaultValue;
+        }
+    }
+}
